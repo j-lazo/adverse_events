fd92497d016d563e13539a1ac297ca132b914907
j-lazo <j.org.ls@ciencias.unam.mx>
2023-12-18 16:18:29 +0100

    added conf matrix analysis functions

---
+++ scripts/utils/data_analysis.py
@@ -0,0 +1,81 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import tqdm
+from sklearn.metrics import recall_score
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import cohen_kappa_score
+from sklearn.metrics import matthews_corrcoef
+from sklearn.metrics import f1_score
+import pandas as pd
+import seaborn as sns
+import collections
+import os
+
+
+def compute_confusion_matrix(gt_data, predicted_data, plot_figure=False, dir_save_fig=''):
+    """
+    Compute the confusion Matrix given the ground-truth data (gt_data) and predicted data (predicted_data)
+    in list format. If Plot is True shows the matrix .
+    Parameters
+    ----------
+    gt_data : list
+    predicted_data : list
+    plot_figure :
+    dir_save_fig :
+
+    Returns
+    -------
+
+    """
+    uniques_predicts = np.unique(predicted_data)
+    uniques_gt = np.unique(gt_data)
+    if collections.Counter(uniques_gt) == collections.Counter(uniques_predicts):
+        uniques = uniques_gt
+    else:
+        uniques = np.unique([*uniques_gt, *uniques_predicts])
+
+    ocurrences = [gt_data.count(unique) for unique in uniques]
+    conf_matrix = confusion_matrix(gt_data, predicted_data)
+    group_percentages = [conf_matrix[i]/ocurrences[i] for i, row in enumerate(conf_matrix)]
+
+    size = len(list(uniques))
+    list_uniques = list(uniques)
+    xlabel_names = list()
+    for name in list_uniques:
+        # if the name of the unique names is longer than 4 characters will split it
+        if len(name) > 4:
+            name_split = name.split('-')
+            new_name = ''
+            for splits in name_split:
+                new_name = new_name.join([splits[0]])
+
+            xlabel_names.append(new_name)
+        else:
+            xlabel_names.append(name)
+
+    labels = np.asarray(group_percentages).reshape(size, size)
+    sns.heatmap(group_percentages, cmap='Blues', cbar=False, linewidths=.5,
+                yticklabels=list(uniques), xticklabels=list(xlabel_names), annot=labels)
+    plt.xlabel('Predicted Class')
+    plt.ylabel('Real Class')
+
+    if plot_figure is True:
+        plt.show()
+
+    if dir_save_fig == '':
+            dir_save_figure = os.getcwd() + '/confusion_matrix.png'
+
+    else:
+        if not dir_save_fig.endswith('.png'):
+          dir_save_figure = dir_save_fig + 'confusion_matrix.png'
+        else:
+            dir_save_figure = dir_save_fig
+
+    print(f'figure saved at: {dir_save_figure}')
+
+    plt.savefig(dir_save_figure)
+    plt.close()
+
+    return conf_matrix
