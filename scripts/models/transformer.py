0ce228f828072c9749f0ea21af1adeea5a783599
j-lazo <j.org.ls@ciencias.unam.mx>
2023-12-19 17:55:40 +0100

    transoermer functions

---
+++ scripts/models/transformer.py
@@ -0,0 +1,80 @@
+import keras
+from keras import layers
+from keras import ops
+import matplotlib.pyplot as plt
+import numpy as np
+class Patches(layers.Layer):
+    def __init__(self, patch_size):
+        super().__init__()
+        self.patch_size = patch_size
+
+    def call(self, images):
+        input_shape = ops.shape(images)
+        batch_size = input_shape[0]
+        height = input_shape[1]
+        width = input_shape[2]
+        channels = input_shape[3]
+        num_patches_h = height // self.patch_size
+        num_patches_w = width // self.patch_size
+        patches = keras.ops.image.extract_patches(images, size=self.patch_size)
+        patches = ops.reshape(
+            patches,
+            (
+                batch_size,
+                num_patches_h * num_patches_w,
+                self.patch_size * self.patch_size * channels,
+            ),
+        )
+        return patches
+
+    def get_config(self):
+        config = super().get_config()
+        config.update({"patch_size": self.patch_size})
+        return config
+
+
+def visualize_patches(x_train, patch_size, image_size):
+    plt.figure(figsize=(4, 4))
+    image = x_train[np.random.choice(range(x_train.shape[0]))]
+    plt.imshow(image.astype("uint8"))
+    plt.axis("off")
+
+    resized_image = ops.image.resize(
+        ops.convert_to_tensor([image]), size=(image_size, image_size)
+    )
+    patches = Patches(patch_size)(resized_image)
+    print(f"Image size: {image_size} X {image_size}")
+    print(f"Patch size: {patch_size} X {patch_size}")
+    print(f"Patches per image: {patches.shape[1]}")
+    print(f"Elements per patch: {patches.shape[-1]}")
+
+    n = int(np.sqrt(patches.shape[1]))
+    plt.figure(figsize=(4, 4))
+    for i, patch in enumerate(patches[0]):
+        ax = plt.subplot(n, n, i + 1)
+        patch_img = ops.reshape(patch, (patch_size, patch_size, 3))
+        plt.imshow(ops.convert_to_numpy(patch_img).astype("uint8"))
+        plt.axis("off")
+
+
+class PatchEncoder(layers.Layer):
+    def __init__(self, num_patches, projection_dim):
+        super().__init__()
+        self.num_patches = num_patches
+        self.projection = layers.Dense(units=projection_dim)
+        self.position_embedding = layers.Embedding(
+            input_dim=num_patches, output_dim=projection_dim
+        )
+
+    def call(self, patch):
+        positions = ops.expand_dims(
+            ops.arange(start=0, stop=self.num_patches, step=1), axis=0
+        )
+        projected_patches = self.projection(patch)
+        encoded = projected_patches + self.position_embedding(positions)
+        return encoded
+
+    def get_config(self):
+        config = super().get_config()
+        config.update({"num_patches": self.num_patches})
+        return config
