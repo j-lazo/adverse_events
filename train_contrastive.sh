9b7539ec6b36cbc26c22e5528ed50066021df1e5
j-lazo <j.org.ls@ciencias.unam.mx>
2024-02-14 11:11:44 +0100

    contrative training

---
+++ train_contrastive.sh
@@ -0,0 +1,5 @@
+source /home/lazojorge/Desktop/current_work/.tensorflow2/bin/activate
+python scripts/train_contrastive.py --path_pickle_train=/home/lazojorge/Desktop/current_work/scripts/adverse_events/data/stras_70/train/1fps_100_0.pickle \
+--path_pickle_val=/home/lazojorge/Desktop/current_work/scripts/adverse_events/data/stras_70/val/1fps_0.pickle \
+--path_dataset=/media/lazojorge/DATA1/iae_frames/ --data_center=stras
+
